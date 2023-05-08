cc5df025f6de3f2df06c6a4ba4fd1ea198ada0a8
Zac McWilliam <zac@mcwilliam.net.au>
2023-05-08 13:29:03 +0100

    Reduce repeated code, add calibration stuff

--- test_linearray.py
+++ test_linearray.py
@@ -5,48 +5,54 @@ import adafruit_ads1x15.ads1015 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 
 i2c = busio.I2C(board.SCL, board.SDA)
+ads_addresses = [0x49, 0x48, 0x4B]
+ads_gain = 1
+ads_mode = ADS.Mode.SINGLE
 
-ads_1 = ADS.ADS1015(i2c, address=0x48)
-ads_1.gain = 1
-ads_1.mode = ADS.Mode.SINGLE
+PORT_ADS_LINE = ["A0", "A1", "A2", "A3", "B0", "B1", "B2", "B3", "C0", "C1", "C2", "C3"]
 
-ads_2 = ADS.ADS1015(i2c, address=0x49)
-ads_1.gain = 1
-ads_2.mode = ADS.Mode.SINGLE
+ads = []
+for address in ads_addresses:
+    ads_instance = ADS.ADS1015(i2c, address=address)
+    ads_instance.gain = ads_gain
+    ads_instance.mode = ads_mode
+    ads.append(ads_instance)
 
-ads_3 = ADS.ADS1015(i2c, address=0x4B)
-ads_1.gain = 1
-ads_3.mode = ADS.Mode.SINGLE
+channels = []
+for ads_port in PORT_ADS_LINE:
+    ads_instance = ads[ord(ads_port[0]) - ord("A")]
+    channel = AnalogIn(ads_instance, getattr(ADS, f"P{ads_port[1]}"))
+    channels.append(channel)
+    
+# cal_min = None
+# cal_max = None
 
-chan_1_0 = AnalogIn(ads_1, ADS.P0)
-chan_1_1 = AnalogIn(ads_1, ADS.P1)
-chan_1_2 = AnalogIn(ads_1, ADS.P2)
-chan_1_3 = AnalogIn(ads_1, ADS.P3)
+# while True:
+#     vals = [channel.value for channel in channels]
 
-chan_2_0 = AnalogIn(ads_2, ADS.P0)
-chan_2_1 = AnalogIn(ads_2, ADS.P1)
-chan_2_2 = AnalogIn(ads_2, ADS.P2)
-chan_2_3 = AnalogIn(ads_2, ADS.P3)
+#     if cal_min is None:
+#         cal_min = vals[:]
+#         cal_max = vals[:]
 
-chan_3_0 = AnalogIn(ads_3, ADS.P0)
-chan_3_1 = AnalogIn(ads_3, ADS.P1)
-chan_3_2 = AnalogIn(ads_3, ADS.P2)
-chan_3_3 = AnalogIn(ads_3, ADS.P3)
+#     for i in range(len(vals)):
+#         if vals[i] < cal_min[i]:
+#             cal_min[i] = vals[i]
+#         if vals[i] > cal_max[i]:
+#             cal_max[i] = vals[i]
+        
+#     print(vals, cal_min, cal_max)
+
+las_min = [1312, 1248, 1280, 1280, 1248, 1280, 1264, 1312, 1296, 1248, 1264, 1344] 
+las_max = [10352, 9600, 10736, 10944, 8544, 11264, 10480, 11952, 10848, 8080, 9584, 12800]
 
 frames = 0
 start = time.time()
 while True:
     frames += 1
-    print(f"{int(chan_1_0.value)} "
-        + f"{int(chan_1_1.value)} "
-        + f"{int(chan_1_2.value)} "
-        + f"{int(chan_1_3.value)} "
-        + f"{int(chan_2_0.value)} "
-        + f"{int(chan_2_1.value)} "
-        + f"{int(chan_2_2.value)} "
-        + f"{int(chan_2_3.value)} "
-        + f"{int(chan_3_0.value)} "
-        + f"{int(chan_3_1.value)} "
-        + f"{int(chan_3_2.value)} "
-        + f"{int(chan_3_3.value)} "
-        + f"\tFPS: {int(frames / (time.time() - start))}")
+    vals = [channel.value for channel in channels]
+    vals = [int((vals[i] - las_min[i]) / (las_max[i] - las_min[i]) * 100) for i in range(len(vals))]
+
+    values_str = "\t".join([f"{val:4d}" for val in vals])
+
+    fps_str = f"\tFPS: {int(frames / (time.time() - start))}"
+    print(values_str + fps_str)
