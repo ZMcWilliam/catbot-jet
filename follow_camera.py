cab2357a4b324c1072fc3f2bfbb728c9544e3f16
Zac McWilliam <zac@mcwilliam.net.au>
2023-05-25 09:19:41 +0100

    Temp remove main loop from a function

--- follow_camera.py
+++ follow_camera.py
@@ -271,161 +271,159 @@ green_s_max_slider.bind("<ButtonRelease-1>", on_slider_change)
 green_v_max_slider.bind("<ButtonRelease-1>", on_slider_change)
     
 # MAIN LOOP
-def main_program():
-    while True:
-        changed_black_contour = False
-        if frames % 20 == 0 and frames != 0:
-            print(f"Processing FPS: {20/(time.time()-fpsTime)}")
-            fpsTime = time.time()
-        # if frames % 100 == 0:
-        #     print(f"Camera 0 average FPS: {cams.get_fps(0)}")
-        img0 = cams.read_stream(0)
-        # cv2.imwrite("testImg.jpg", img0)
-        if (img0 is None):
-            continue
-        img0 = img0.copy()
+# def main_program():
+while True:
+    changed_black_contour = False
+    if frames % 20 == 0 and frames != 0:
+        print(f"Processing FPS: {20/(time.time()-fpsTime)}")
+        fpsTime = time.time()
+    # if frames % 100 == 0:
+    #     print(f"Camera 0 average FPS: {cams.get_fps(0)}")
+    img0 = cams.read_stream(0)
+    # cv2.imwrite("testImg.jpg", img0)
+    if (img0 is None):
+        continue
+    img0 = img0.copy()
 
-        img0 = img0[0:img0.shape[0]-38, 0:img0.shape[1]-70]
-        img0 = cv2.cvtColor(img0, cv2.COLOR_BGR2RGB)
+    img0 = img0[0:img0.shape[0]-38, 0:img0.shape[1]-70]
+    img0 = cv2.cvtColor(img0, cv2.COLOR_BGR2RGB)
 
-        # #Find the black in the image
-        img0_gray = cv2.cvtColor(img0, cv2.COLOR_RGB2GRAY)
-        # img0_gray = cv2.equalizeHist(img0_gray)
-        img0_gray = cv2.GaussianBlur(img0_gray, (5, 5), 0)
+    # #Find the black in the image
+    img0_gray = cv2.cvtColor(img0, cv2.COLOR_RGB2GRAY)
+    # img0_gray = cv2.equalizeHist(img0_gray)
+    img0_gray = cv2.GaussianBlur(img0_gray, (5, 5), 0)
 
-        img0_gray_scaled = 255 / np.clip(calibration_map, a_min=1, a_max=None) * img0_gray  # Scale white values based on the inverse of the calibration map
-        img0_gray_scaled = np.clip(img0_gray_scaled, 0, 255)    # Clip the scaled image to ensure values are within the valid range
-        img0_gray_scaled = img0_gray_scaled.astype(np.uint8)    # Convert the scaled image back to uint8 data type
+    img0_gray_scaled = 255 / np.clip(calibration_map, a_min=1, a_max=None) * img0_gray  # Scale white values based on the inverse of the calibration map
+    img0_gray_scaled = np.clip(img0_gray_scaled, 0, 255)    # Clip the scaled image to ensure values are within the valid range
+    img0_gray_scaled = img0_gray_scaled.astype(np.uint8)    # Convert the scaled image back to uint8 data type
 
-        img0_binary = cv2.threshold(img0_gray_scaled, config_values["black_line_threshold"][0], config_values["black_line_threshold"][1], cv2.THRESH_BINARY)[1]
-        img0_binary = cv2.morphologyEx(img0_binary, cv2.MORPH_OPEN, np.ones((7,7),np.uint8))
+    img0_binary = cv2.threshold(img0_gray_scaled, config_values["black_line_threshold"][0], config_values["black_line_threshold"][1], cv2.THRESH_BINARY)[1]
+    img0_binary = cv2.morphologyEx(img0_binary, cv2.MORPH_OPEN, np.ones((7,7),np.uint8))
 
-        img0_hsv = cv2.cvtColor(img0, cv2.COLOR_RGB2HSV)
+    img0_hsv = cv2.cvtColor(img0, cv2.COLOR_RGB2HSV)
 
-        #Find the green in the image
-        img0_green = cv2.bitwise_not(cv2.inRange(img0_hsv, config_values["green_turn_hsv_threshold"][0], config_values["green_turn_hsv_threshold"][1]))
-        img0_green = cv2.erode(img0_green, np.ones((5,5),np.uint8), iterations=1)
+    #Find the green in the image
+    img0_green = cv2.bitwise_not(cv2.inRange(img0_hsv, config_values["green_turn_hsv_threshold"][0], config_values["green_turn_hsv_threshold"][1]))
+    img0_green = cv2.erode(img0_green, np.ones((5,5),np.uint8), iterations=1)
 
-        # #Remove the green from the black (since green looks like black when grayscaled)
-        img0_line = cv2.dilate(img0_binary, np.ones((5,5),np.uint8), iterations=2)
-        img0_line = cv2.bitwise_or(img0_binary, cv2.bitwise_not(img0_green))
+    # #Remove the green from the black (since green looks like black when grayscaled)
+    img0_line = cv2.dilate(img0_binary, np.ones((5,5),np.uint8), iterations=2)
+    img0_line = cv2.bitwise_or(img0_binary, cv2.bitwise_not(img0_green))
 
-        # -----------
+    # -----------
 
-        raw_white_contours, white_hierarchy = cv2.findContours(img0_binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    raw_white_contours, white_hierarchy = cv2.findContours(img0_binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        # Filter white contours based on area
-        white_contours = []
-        for contour in raw_white_contours:
-            if (cv2.contourArea(contour) > 1000):
-                white_contours.append(contour)
-        img0_binary_not = cv2.bitwise_not(img0_binary)
-        
-        # Find black contours
-        # If there are no black contours, skip the rest of the loop
-        black_contours, black_hierarchy = cv2.findContours(img0_binary_not, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        if (len(black_contours) == 0):
-            print("No black contours found")
-            continue
-        
-        # -----------
-        # GREEN TURNS
-        # -----------
+    # Filter white contours based on area
+    white_contours = []
+    for contour in raw_white_contours:
+        if (cv2.contourArea(contour) > 1000):
+            white_contours.append(contour)
+    img0_binary_not = cv2.bitwise_not(img0_binary)
+    
+    # Find black contours
+    # If there are no black contours, skip the rest of the loop
+    black_contours, black_hierarchy = cv2.findContours(img0_binary_not, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    if (len(black_contours) == 0):
+        print("No black contours found")
+        continue
+    
+    # -----------
+    # GREEN TURNS
+    # -----------
 
-        is_there_green = np.count_nonzero(img0_green == 0)
-        turning = False
-        black_contours_turn = None
+    is_there_green = np.count_nonzero(img0_green == 0)
+    turning = False
+    black_contours_turn = None
 
-        # print("Green: ", is_there_green)
-        
-        # Check if there is a significant amount of green pixels
-        if is_there_green > 2000: #and len(white_contours) > 2: #((is_there_green > 1000 or time.time() - last_green_found_time < 0.5) and (len(white_contours) > 2 or greenCenter is not None)):
-            unfiltered_green_contours, green_hierarchy = cv2.findContours(cv2.bitwise_not(img0_green), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            # cv2.drawContours(img0, green_contours[0], -1, (255,255,0), 3)
-
-            # TODO
-
-            print("GREEN TURN STUFF")
-        else:
-            greenCenter = None # Reset greenturn memory if no green found!
-
-
-        # -----------
-        # INTERSECTIONS
-        # -----------
+    # print("Green: ", is_there_green)
+    
+    # Check if there is a significant amount of green pixels
+    if is_there_green > 2000: #and len(white_contours) > 2: #((is_there_green > 1000 or time.time() - last_green_found_time < 0.5) and (len(white_contours) > 2 or greenCenter is not None)):
+        unfiltered_green_contours, green_hierarchy = cv2.findContours(cv2.bitwise_not(img0_green), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        # cv2.drawContours(img0, green_contours[0], -1, (255,255,0), 3)
 
         # TODO
 
-        # -----------
-        # REST OF LINE LINE FOLLOWER
-        # -----------
-
-        #Find the black contours
-        sorted_black_contours = FindBestContours(black_contours)
-        if (len(sorted_black_contours) == 0):
-            print("No black contours found")
-
-            print("STEER TEMP: GO FORWARD")
-            continue
-        chosen_black_contour = sorted_black_contours[0]
-
-        cv2.drawContours(img0, [chosen_black_contour[2]], -1, (0,255,0), 3) # DEBUG
-        
-        # Update the reference position for subsequent calculations
-        last_line_pos = np.array([chosen_black_contour[1][0][0], chosen_black_contour[1][0][1]])
+        print("GREEN TURN STUFF")
+    else:
+        greenCenter = None # Reset greenturn memory if no green found!
 
 
+    # -----------
+    # INTERSECTIONS
+    # -----------
 
-        preview_image_img0 = cv2.resize(img0, (0,0), fx=0.8, fy=0.7)
-        cv2.imshow("img0", preview_image_img0)
+    # TODO
 
-        preview_image_img0_binary = cv2.resize(img0_binary, (0,0), fx=0.8, fy=0.7)
-        cv2.imshow("img0_binary", preview_image_img0_binary)
+    # -----------
+    # REST OF LINE LINE FOLLOWER
+    # -----------
 
-        preview_image_img0_line = cv2.resize(img0_line, (0,0), fx=0.8, fy=0.7)
-        cv2.imshow("img0_line", preview_image_img0_line)
+    #Find the black contours
+    sorted_black_contours = FindBestContours(black_contours)
+    if (len(sorted_black_contours) == 0):
+        print("No black contours found")
 
-        preview_image_img0_green = cv2.resize(img0_green, (0,0), fx=0.8, fy=0.7)
-        cv2.imshow("img0_green", preview_image_img0_green)
+        print("STEER TEMP: GO FORWARD")
+        continue
+    chosen_black_contour = sorted_black_contours[0]
 
-        # preview_image_img0_gray = cv2.resize(img0_gray, (0,0), fx=0.8, fy=0.7)
-        # cv2.imshow("img0_gray", preview_image_img0_gray)
+    cv2.drawContours(img0, [chosen_black_contour[2]], -1, (0,255,0), 3) # DEBUG
+    
+    # Update the reference position for subsequent calculations
+    last_line_pos = np.array([chosen_black_contour[1][0][0], chosen_black_contour[1][0][1]])
 
-        # def mouseCallbackHSV(event, x, y, flags, param):
-        #     if event == cv2.EVENT_MOUSEMOVE and flags == cv2.EVENT_FLAG_LBUTTON:
-        #         # Print HSV value only when the left mouse button is pressed and mouse is moving
-        #         hsv_value = img0_hsv[y, x]
-        #         print(f"HSV: {hsv_value}")
-        # # Show HSV preview with text on hover to show HSV values
-        # preview_image_img0_hsv = cv2.resize(img0_hsv, (0,0), fx=0.8, fy=0.7)
-        # cv2.imshow("img0_hsv", preview_image_img0_hsv)
-        # cv2.setMouseCallback("img0_hsv", mouseCallbackHSV)
+    preview_image_img0 = cv2.resize(img0, (0,0), fx=0.8, fy=0.7)
+    cv2.imshow("img0", preview_image_img0)
 
-        preview_image_img0_gray_scaled = cv2.resize(img0_gray_scaled, (0,0), fx=0.8, fy=0.7)
-        cv2.imshow("img0_gray_scaled", preview_image_img0_gray_scaled)
+    preview_image_img0_binary = cv2.resize(img0_binary, (0,0), fx=0.8, fy=0.7)
+    cv2.imshow("img0_binary", preview_image_img0_binary)
 
-        # Show a preview of the image with the contours drawn on it, black as red and white as blue
+    preview_image_img0_line = cv2.resize(img0_line, (0,0), fx=0.8, fy=0.7)
+    cv2.imshow("img0_line", preview_image_img0_line)
 
-        preview_image_img0_contours = img0.copy()
-        cv2.drawContours(preview_image_img0_contours, white_contours, -1, (255,0,0), 3)
-        cv2.drawContours(preview_image_img0_contours, black_contours, -1, (0,0,255), 3)
-        preview_image_img0_contours = cv2.resize(preview_image_img0_contours, (0,0), fx=0.8, fy=0.7)
-        cv2.imshow("img0_contours", preview_image_img0_contours)
+    preview_image_img0_green = cv2.resize(img0_green, (0,0), fx=0.8, fy=0.7)
+    cv2.imshow("img0_green", preview_image_img0_green)
 
-        # frames += 1
+    # preview_image_img0_gray = cv2.resize(img0_gray, (0,0), fx=0.8, fy=0.7)
+    # cv2.imshow("img0_gray", preview_image_img0_gray)
 
-        k = cv2.waitKey(1)
-        if (k & 0xFF == ord('q')):
-            # pr.print_stats(SortKey.TIME)
-            program_active = False
-            break
+    # def mouseCallbackHSV(event, x, y, flags, param):
+    #     if event == cv2.EVENT_MOUSEMOVE and flags == cv2.EVENT_FLAG_LBUTTON:
+    #         # Print HSV value only when the left mouse button is pressed and mouse is moving
+    #         hsv_value = img0_hsv[y, x]
+    #         print(f"HSV: {hsv_value}")
+    # # Show HSV preview with text on hover to show HSV values
+    # preview_image_img0_hsv = cv2.resize(img0_hsv, (0,0), fx=0.8, fy=0.7)
+    # cv2.imshow("img0_hsv", preview_image_img0_hsv)
+    # cv2.setMouseCallback("img0_hsv", mouseCallbackHSV)
 
-    cams.stop()
+    preview_image_img0_gray_scaled = cv2.resize(img0_gray_scaled, (0,0), fx=0.8, fy=0.7)
+    cv2.imshow("img0_gray_scaled", preview_image_img0_gray_scaled)
+
+    # Show a preview of the image with the contours drawn on it, black as red and white as blue
+
+    preview_image_img0_contours = img0_clean.copy()
+    cv2.drawContours(preview_image_img0_contours, white_contours, -1, (255,0,0), 3)
+    cv2.drawContours(preview_image_img0_contours, black_contours, -1, (0,0,255), 3)
+    preview_image_img0_contours = cv2.resize(preview_image_img0_contours, (0,0), fx=0.8, fy=0.7)
+    cv2.imshow("img0_contours", preview_image_img0_contours)
+
+    # frames += 1
+
+    k = cv2.waitKey(1)
+    if (k & 0xFF == ord('q')):
+        # pr.print_stats(SortKey.TIME)
+        program_active = False
+        break
+
+cams.stop()
 
 # Start the main program logic in a new thread
-program_thread = threading.Thread(target=main_program)
-program_thread.daemon = True
-program_thread.start()
+# program_thread = threading.Thread(target=main_program)
+# program_thread.daemon = True
+# program_thread.start()
 
 # Start the Tkinter UI main loop
 # root.mainloop()
