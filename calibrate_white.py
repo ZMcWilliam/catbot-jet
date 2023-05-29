7a8b421d7e69840c2faae97f8fc3b95d06d7fd18
Zac McWilliam <zac@mcwilliam.net.au>
2023-05-29 07:57:30 +0100

    Add black calibration, improve test cam script

--- calibrate_white.py
+++ calibrate_white.py
@@ -1,4 +1,5 @@
 import json
+import time
 import cv2
 import helper_camera
 import numpy as np
@@ -6,37 +7,76 @@ import numpy as np
 cams = helper_camera.CameraController()
 cams.start_stream(0)
 
-calibration_images = []
-calibration_value = 0
+calibration_images = {
+    "w": [],
+    "b": [],
+}
 
-NUM_CALIBRATION_IMAGES = 10
+NUM_CALIBRATION_IMAGES = 50
 
-while NUM_CALIBRATION_IMAGES > len(calibration_images):
-    img = cams.read_stream(0)
-    if img is None:
+time.sleep(1)
+
+# Load the calibration map from the JSON file if it exists
+calibration_data = {
+    "calibration_value_w": 0, 
+    "calibration_value_b": 0,
+    "calibration_map_w": [],
+    "calibration_map_b": [],
+}
+try:
+    with open("calibration.json", "r") as json_file:
+        calibration_data = json.load(json_file)
+except:
+    pass # If the file doesn't exist, we'll create it later
+
+while True:
+    requested = input("Enter 'w' for white calibration, 'b' for black calibration, or 'q' to quit: ")
+    
+    if requested == "q":
+        break
+
+    if requested not in ["w", "b"]:
         continue
-    img = img[0:img.shape[0]-38, 0:img.shape[1]-70]
+
+    calibration_images[requested] = []
+
+    while NUM_CALIBRATION_IMAGES > len(calibration_images[requested]):
+        img = cams.read_stream(0)
+        if img is None:
+            continue
+        img = img[0:img.shape[0]-38, 0:img.shape[1]-70]
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        img_gray = cv2.GaussianBlur(img_gray, (5, 5), 0)
+        
+        print(img_gray[100][100])
+        
+        calibration_images[requested].append(img_gray)
+        print(f"Calibration image {len(calibration_images[requested])} of {NUM_CALIBRATION_IMAGES} captured.")
+
+        time.sleep(0.01)
+        cv2.imshow("Calibration Image", img_gray)
+        k = cv2.waitKey(1)
+        if (k & 0xFF == ord('q')):
+            break
+
+    # Calculate the average grayscale value across all calibration images
+    calibration_data["calibration_value_" + requested] = np.mean([np.mean(img_gray) for img_gray in calibration_images[requested]])
+    # Create an empty calibration map
+    calibration_map = np.zeros_like(calibration_images[requested][0], dtype=np.float32)
+    # Calculate the calibration map
+    for img_gray in calibration_images[requested]:
+        calibration_map += img_gray
+
+    calibration_map //= len(calibration_images[requested])
+
+    calibration_data["calibration_map_" + requested] = calibration_map.tolist()
+
+    # Save the calibration map to the JSON file
+    with open("calibration.json", "w") as json_file:
+        json.dump(calibration_data, json_file, indent=4)
+
+    print("Calibration images captured, updated calibration.json")
     
-    img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    img_gray = cv2.GaussianBlur(img_gray, (5, 5), 0)
-    
-    calibration_images.append(img_gray)
-    print(f"Calibration image {len(calibration_images)} of {NUM_CALIBRATION_IMAGES} captured.")
-    
-# Calculate the average grayscale value across all calibration images
-calibration_value = np.mean([np.mean(img_gray) for img_gray in calibration_images])
-# Create an empty calibration map
-calibration_map = np.zeros_like(calibration_images[0], dtype=np.float32)
-# Calculate the calibration map
-for img_gray in calibration_images:
-    calibration_map += img_gray
-
-calibration_map //= len(calibration_images)
-
-calibration_data = {"calibration_value": calibration_value, "calibration_map": calibration_map.tolist()}
-with open("calibration.json", "w") as json_file:
-    json.dump(calibration_data, json_file)
-
-print(f"Calibration value: {calibration_value}")
-
 cams.stop()
--- test_camera.py
+++ test_camera.py
@@ -1,76 +1,56 @@
-from picamera2 import Picamera2
 import time
 import cv2
+import json
+import helper_camera
+import numpy as np
 from threading import Thread
+from picamera2 import Picamera2
 
-def get_camera(num):
-    cam = Picamera2(num)
-    return cam
+cams = helper_camera.CameraController()
+cams.start_stream(0)
+time.sleep(1)
 
-class CameraController:
-    def __init__(self):
-        self.cameras = [CameraStream(0)]
+# Load the calibration map from the JSON file
+with open("calibration.json", "r") as json_file:
+    calibration_data = json.load(json_file)
+calibration_map_black = np.array(calibration_data["calibration_map_b"], dtype=np.float32)
+calibration_map_white = np.array(calibration_data["calibration_map_w"], dtype=np.float32)
 
-    def stop(self):
-        for cam in self.cameras:
-            cam.stop_stream()
+while True:
+    img0 = cams.read_stream(0)
+    img0 = img0[0:img0.shape[0]-38, 0:img0.shape[1]-70]
+    img0 = cv2.cvtColor(img0, cv2.COLOR_BGR2RGB)
 
-    def read_stream(self, num):
-        return self.cameras[num].read_stream()
+    img0_gray = cv2.cvtColor(img0, cv2.COLOR_RGB2GRAY)
+    # img0_gray = cv2.equalizeHist(img0_gray)
+    img0_gray = cv2.GaussianBlur(img0_gray, (5, 5), 0)
 
-    def start_stream(self, num):
-        self.cameras[num].start_stream()
+    print(img0_gray[100][100], calibration_map_black[100][100], calibration_map_white[100][100])
 
-    def stop_stream(self, num):
-        self.cameras[num].stop_stream()
+    # img0_gray_scaled = 240 / np.clip(calibration_map, a_min=1, a_max=None) * img0_gray  # Scale white values based on the inverse of the calibration map
+    # img0_gray_scaled = np.clip(img0_gray_scaled, 0, 255)    # Clip the scaled image to ensure values are within the valid range
+    # img0_gray_scaled = img0_gray_scaled.astype(np.uint8)    # Convert the scaled image back to uint8 data type
 
-class CameraStream:
-    def __init__(self, num=0):
-        self.num = num
-        self.cam = get_camera(self.num)
+    # Scale black values
+    img0_gray_scaled_black = img0_gray / np.clip(calibration_map_black, a_min=1, a_max=None) * 255
+    img0_gray_scaled_black = np.clip(img0_gray_scaled_black, 0, 255).astype(np.uint8)
 
-        self.stream_running = False
-        self.frame = None
-        
-    def read_stream(self):
-        if not self.stream_running: 
-            raise Exception(f"Camera {self.num} is not active, run .start_stream() first")
+    # Scale white values
+    img0_gray_scaled_white = 255 / np.clip(calibration_map_white, a_min=1, a_max=None) * img0_gray
+    img0_gray_scaled_white = np.clip(img0_gray_scaled_white, 0, 255).astype(np.uint8)
 
-        return self.frame
+    # Combine the scaled black and white values
+    img0_gray_scaled = img0_gray_scaled_black + img0_gray_scaled_white
+    img0_gray_scaled = np.clip(img0_gray_scaled, 0, 255).astype(np.uint8)
 
-    def start_stream(self):
-        print(f"[CAMERA] Starting stream for Camera {self.num}")
-        self.thread = Thread(target=self.update_stream, args=())
-        self.stream_running = True
-        self.thread.start()
-        
-    def update_stream(self):
-        self.cam.start()
+    cv2.imshow(f"img0", img0)
+    cv2.imshow(f"img0_gray", img0_gray)
+    cv2.imshow(f"img0_gray_scaled", img0_gray_scaled)
+    cv2.imshow(f"img0_gray_scaled_black", img0_gray_scaled_black)
+    cv2.imshow(f"img0_gray_scaled_white", img0_gray_scaled_white)
 
-        while self.stream_running:
-            self.frame = self.cam.helpers.make_array(self.cam.capture_buffer(), self.cam.camera_configuration()["main"])
-        
-        self.cam.stop()
+    k = cv2.waitKey(1)
+    if (k & 0xFF == ord('q')):
+        break
 
-    def stop_stream(self):
-        print(f"[CAMERA] Stopping stream for Camera {self.num}")
-        self.stream_running = False
-
-if __name__ == "__main__":
-    camNum = 0
-    cams = CameraController()
-    cams.start_stream(camNum)
-    time.sleep(1)
-    
-    while True:
-        img1 = cams.read_stream(camNum)
-        img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
-        # img1 = cv2.resize(img1, (0,0), fx=0.5, fy=0.5)
-
-        cv2.imshow(f"Camera {camNum}", img1)
-
-        k = cv2.waitKey(1)
-        if (k & 0xFF == ord('q')):
-            break
-    
-    cams.stop()
+cams.stop()
