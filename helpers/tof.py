d90ba69b938dca4563bda485659c513c21d7d267
Zac McWilliam <zac@mcwilliam.net.au>
2023-11-30 13:05:41 +1100

    Replace VL6180X with VL53L1X

--- helpers/tof.py
+++ helpers/tof.py
@@ -2,21 +2,33 @@ import threading
 import time
 import board
 import busio
-import adafruit_vl6180x
+import adafruit_vl53l1x
 
 class RangeSensorMonitor(threading.Thread):
     def __init__(self):
         super().__init__()
-
         self.i2c = busio.I2C(board.SCL, board.SDA)
-        self.sensor = adafruit_vl6180x.VL6180X(self.i2c)
-        self.range_mm = None
+        self.sensor = adafruit_vl53l1x.VL53L1X(self.i2c)
+        self.range_mm = 0
         self._stop_event = threading.Event()
 
+        self.start_time = time.time()
+        self.frames = 0
+
     def run(self):
+        self.sensor.start_ranging()
         while not self._stop_event.is_set():
-            self.range_mm = self.sensor.range
-            time.sleep(0.1)
+            if self.sensor.data_ready:
+                new_dist = self.sensor.distance
+                if new_dist is not None:
+                    self.range_mm = new_dist * 10
+                self.sensor.clear_interrupt()
+                self.frames += 1
+        self.sensor.stop_ranging()
 
     def stop(self):
         self._stop_event.set()
+    
+    @property
+    def fps(self):
+        return int(self.frames / (time.time() - self.start_time))
--- tests/tof.py
+++ tests/tof.py
@@ -1,4 +1,4 @@
-import adafruit_vl6180x
+import time
 from helpers.tof import RangeSensorMonitor
 
 tof = RangeSensorMonitor()
@@ -6,17 +6,9 @@ tof.start()
 
 try:
     while True:
-        # - adafruit_vl6180x.ALS_GAIN_1 = 1x
-        # - adafruit_vl6180x.ALS_GAIN_1_25 = 1.25x
-        # - adafruit_vl6180x.ALS_GAIN_1_67 = 1.67x
-        # - adafruit_vl6180x.ALS_GAIN_2_5 = 2.5x
-        # - adafruit_vl6180x.ALS_GAIN_5 = 5x
-        # - adafruit_vl6180x.ALS_GAIN_10 = 10x
-        # - adafruit_vl6180x.ALS_GAIN_20 = 20x
-        # - adafruit_vl6180x.ALS_GAIN_40 = 40x
-        light_lux = tof.sensor.read_lux(adafruit_vl6180x.ALS_GAIN_1)
         range_mm = tof.range_mm
-        print(f"{range_mm}mm {light_lux}lux")
+        print(f"{range_mm:4.0f}mm @ {tof.fps:3.0f}fps")
+        time.sleep(0.1)
 except KeyboardInterrupt:
     tof.stop()
     tof.join()
